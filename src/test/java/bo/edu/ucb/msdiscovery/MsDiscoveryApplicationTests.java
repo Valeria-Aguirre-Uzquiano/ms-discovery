1a4dedf46c697937305e28750936522fe61a4ccd
Valeria-Aguirre-Uzquiano <valeria.aguirre.uzquiano@gmail.com>
2022-05-28 22:26:51 -0400

    subir ms discovery

---
+++ src/test/java/bo/edu/ucb/msdiscovery/MsDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.msdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
